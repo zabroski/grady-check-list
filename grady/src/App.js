2225d052707b66fa9133548101d996d71879c890
Issouf Zabre <sheridanzabre@gmail.com>
2020-01-09 20:44:01 -0500

    app with hard coding

---
+++ grady/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import TodoItem from "./Components/TodoItem"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </div>
+  );
+}
+
+export default App;
