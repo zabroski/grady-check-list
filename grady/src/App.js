04b938e819cc83348c5f1508354b74194867a3af
Issouf Zabre <sheridanzabre@gmail.com>
2020-01-13 21:01:07 -0500

    clean code

--- grady/src/App.js
+++ grady/src/App.js
@@ -4,14 +4,32 @@ import todosData from "./Components/todosData"
 import './App.css';
 
 class  App extends React.Component {
-  constructor() {
-    super()
-    this.state ={
-      todos:todosData
-    }
+
+  state ={
+    todos:todosData
+  }
+ 
+
+  handleChange = (id) => {
+    this.setState(prevState => {
+      const updateTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo
+      })
+      return {
+        todos: updateTodos
+      }
+    })
+
   }
   render(){
-    const todoItems = this.state.todos.map (item => <TodoItem key={item.id} item={item} />)
+    const todoItems = this.state.todos.map (item => <TodoItem key={item.id} item={item}  
+    handleChange={this.handleChange}/>)
     return (
       <div className="App">
         {todoItems}
--- grady/src/Components/TodoItem.js
+++ grady/src/Components/TodoItem.js
@@ -1,10 +1,18 @@
 import React from "react"
 
 function TododItem (props) {
+
+    const compledStyle = {
+        fontStyle: "italic",
+        color: "red",
+        textDecoration: "line-through"
+    }
     return (
         <div className="todo-item" >
-            <input type="checkbox" checked={props.item.completed} />
-            <p>{props.item.text}</p>
+            <input type="checkbox" 
+            checked={props.item.completed} 
+             onChange={() => props.handleChange(props.item.id)}/>
+            <p style={props.item.completed ? compledStyle: null}>{props.item.text}</p>
         </div>
     )
 }
